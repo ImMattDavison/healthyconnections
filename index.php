c2dd48c6f9c047b586c3ba3c7b83095d986184eb
Matt <matt@spectrebot.net>
2022-09-24 13:57:33 +0100

    update checking for login

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php
+    error_reporting(0);
+
     require_once('includes/config.php');
 
-    if($user->is_logged_in() ){?>
+    if($user->is_logged_in()){?>
         <!-- IF LOGGED IN THIS WILL SHOW -->
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta charset="utf-8">
             <title>Home | Healthy Connections</title>
             <link rel="stylesheet" href="style/main.css">
+            <link rel="stylesheet" href="style/dashboard.css">
         </head>
         <?php include('globals/navbar.php') ?>
+
+        <h1>Dashboard</h1>
     <?php } else { ?>
         <!-- IF NOT LOGGED IN THIS WILL SHOW -->
         <head>
