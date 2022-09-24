32db0e5a334afd5e37c89338865d411ff8b107e2
Matt <matt@spectrebot.net>
2022-09-24 22:47:13 +0100

    remove unused echo

--- trial.php
+++ trial.php
@@ -15,12 +15,3 @@ if($gatheredContent['postType'] = 2){
    header('Location: job.php?'.$gatheredContent['postid'].'id='.$postid.'');
 }
  
-
-echo "<div class='post-cell'>
-                                        <div class='post-block'>
-                                            <small class='listed-by'> Listing by: " .$gatheredContent['username']."</small>
-                                            <h4>" . $gatheredContent['trialName'] . "</h4>
-                                            <p>" . $gatheredContent['trialDesc'] . "</p>
-                                            <a class='post-button' href='job.php?id=" . $gatheredContent['postid'] . "'>View Ad</a>
-                                        </div>
-                                    </div>";
