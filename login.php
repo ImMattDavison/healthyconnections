24ab95b17c32c3d762a716019f20a65a6382173a
Matt <matt@spectrebot.net>
2022-09-24 16:50:48 +0100

    fix login

--- login.php
+++ login.php
@@ -1,56 +1,64 @@
-<?php
-//include config
-require_once('includes/config.php');
+<?php 
 
+include 'includes/config.php';
 
-//check if already logged in
-if($user->is_logged_in() ){ header('Location: index.php'); } 
+session_start();
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM site_users WHERE username='$username' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+        $_SESSION['loggedin'] = true;
+		$_SESSION['username'] = $row['username'];
+		header("Location: index.php");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
+
 ?>
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="utf-8">
-  <title>Login | HealthyConnections</title>
-  <link rel="stylesheet" href="style/main.css">
-  <link rel="stylesheet" href="style/login.css">
+	<!-- REQUIRED META -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- STYLE -->
+	<link rel="stylesheet" type="text/css" href="style/login.css">
+	<link rel="stylesheet" type="text/css" href="style/main.css">
+
+	<!-- SEO -->
+	<title>Login | Healthy Connections</title>
 </head>
 <body>
-
-<div class="container">
-
-	<?php
-
-	//process login form if submitted
-	if(isset($_POST['submit'])){
-
-		$username = trim($_POST['username']);
-		$password = trim($_POST['password']);
-		
-		if($user->login($username,$password)){ 
-
-			//logged in return to index page
-			header('Location: index.php');
-			exit;
-		
-
-		} else {
-			$message = '<p class="error">ERROR: Incorrect username or password</p>';
-		}
-
-	}//end if submit
-
-	if(isset($message)){ echo $message; }
-	?>
-	<div class="form-body"> 
-		<h1>Login</h1>
-		<form action="" method="post">
-			<p><label>Username</label><br><input required type="text" name="username" value=""  /></p>
-			<p><label>Password</label><br><input required type="password" name="password" value=""  /></p>
-			<label></label><input class="login" type="submit" name="submit" value="Login"/>
-		</form>
+	<div class="container">
+		<div class="form-body">
+        <h1 class="login-text">Login</h1>
+			<form action="" method="POST" class="login-email">
+				<div class="input-group">
+					<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+				</div>
+				<div class="input-group">
+					<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+				</div>
+				<div class="input-group">
+                    <input class="login" type='submit' name='submit' value='Login'>
+				</div>
+				<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+			</form>
+		</div>
 	</div>
-
-</div>
 </body>
 </html>
