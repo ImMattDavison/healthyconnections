c99c37e879b5ab3956707e6d47981a180cd8629c
Matt <matt@spectrebot.net>
2022-09-24 09:57:13 +0100

    create signup page

---
+++ signup.php
@@ -0,0 +1,103 @@
+<?php //include config
+require_once('includes/config.php');
+
+//if not logged in redirect to login page
+if(!$user->is_logged_in()){ header('Location: login.php'); }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <meta charset="utf-8">
+  <title>Signup | Healthy Connections</title>
+</head>
+<body>
+
+<div class="container">
+
+
+	<h2>Signup</h2>
+
+	<?php
+
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+
+		//collect form data
+		extract($_POST);
+
+		//very basic validation
+		if($username ==''){
+			$error[] = 'Please enter the username.';
+		}
+
+		if($password ==''){
+			$error[] = 'Please enter the password.';
+		}
+
+		if($passwordConfirm ==''){
+			$error[] = 'Please confirm the password.';
+		}
+
+		if($password != $passwordConfirm){
+			$error[] = 'Passwords do not match.';
+		}
+
+		if($email ==''){
+			$error[] = 'Please enter the email address.';
+		}
+
+		if(!isset($error)){
+
+			$hashedpassword = password_hash($password, PASSWORD_BCRYPT);
+
+			try {
+
+				//insert into database
+				$stmt = $db->prepare('INSERT INTO site_users (username,password,email) VALUES (:username, :password, :email)') ;
+				$stmt->execute(array(
+					':username' => $username,
+					':password' => $hashedpassword,
+					':email' => $email
+				));
+
+				//redirect to index page
+				header('Location: users.php?action=added');
+				exit;
+
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}
+
+		}
+
+	}
+
+	//check for any errors
+	if(isset($error)){
+		foreach($error as $error){
+			echo '<p class="error">'.$error.'</p>';
+		}
+	}
+	?>
+
+	<form action='' method='post'>
+
+		<p><label>Username</label><br />
+		<input type='text' name='username' value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
+
+		<p><label>Email</label><br />
+		<input type='text' name='email' value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
+
+		<p><label>Password</label><br />
+		<input type='password' name='password' value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
+
+		<p><label>Confirm Password</label><br />
+		<input type='password' name='passwordConfirm' value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
+		
+		<p><input class="submit" type='submit' name='submit' value='Sign Up'></p>
+
+	</form>
+
+</div>
+</body>
