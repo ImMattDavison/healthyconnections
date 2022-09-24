7aab559800547f420abada4ecb9230cbeec7480c
Matt <matt@spectrebot.net>
2022-09-24 20:27:44 +0100

    structure basic SQL query

---
+++ trial.php
@@ -0,0 +1,12 @@
+<?php require('includes/config.php'); 
+// if not logged in redirect to login page
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header('Location: login.php');
+    exit();
+}
+$postid = $_GET['id'];
+$pgContent = mysqli_query($conn, 'SELECT * FROM posts WHERE postid = $postid');
+
+
