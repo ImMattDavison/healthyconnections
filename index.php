8a4f339ffd3161bc8acba0a13a8b47c685c8122d
Matt <matt@spectrebot.net>
2022-09-24 17:01:13 +0100

    update ordering

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     error_reporting(0);
     require_once('includes/config.php');
     session_start();
-    echo $_SESSION['username'];
-    if(isset($_SESSION['username'])){?>
+    if(!isset($_SESSION['username'])){?>
         <!-- IF LOGGED IN THIS WILL SHOW -->
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta charset="utf-8">
+            <title>Healthy Connections | Connect with Doctors, Find Medical Trials and More.</title>
+            <link rel="stylesheet" href="style/main.css">
+        </head>
+        <body>
+            <?php include('globals/navbar.php') ?>
+            
+        </body>
+    <?php } else if(isset($_SESSION['username'])) { ?>
+        <!-- IF NOT LOGGED IN THIS WILL SHOW -->
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta charset="utf-8">
@@ -17,18 +28,6 @@
         <body>
             <?php include('globals/navbar.php') ?>
             <h1>Dashboard</h1>
-        </body>
-    <?php } else if(!isset($_SESSION['username'])) { ?>
-        <!-- IF NOT LOGGED IN THIS WILL SHOW -->
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta charset="utf-8">
-            <title>Healthy Connections | Connect with Doctors, Find Medical Trials and More.</title>
-            <link rel="stylesheet" href="style/main.css">
-        </head>
-        <body>
-            <?php include('globals/navbar.php') ?>
-            
         </body>
     <?php } ?>
 </html>
