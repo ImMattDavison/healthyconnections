38cd8728b1baca01ddd3f89004a677f597f7ceb4
Matt <matt@spectrebot.net>
2022-09-24 16:51:03 +0100

    fix signup

--- signup.php
+++ signup.php
@@ -1,152 +1,103 @@
-<?php //include config
-require_once('includes/config.php');
+<?php 
 
-//if not logged in redirect to login page
-if($user->is_logged_in()){ header('Location: index.php'); }
+include 'includes/config.php';
+
+// error_reporting(0);
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+$sdrn = random_int(100000, 999999);
+$uid = time().$sdrn;
+
+if (isset($_POST['submit'])) {
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+    $phoneNumber = $_POST['phoneNumber'];
+	$password = md5($_POST['password']);
+	$cpassword = md5($_POST['cpassword']);
+
+	if ($password == $cpassword) {
+		$sql = "SELECT * FROM site_users WHERE email='$email'";
+		$result = mysqli_query($conn, $sql);
+		if (!$result->num_rows > 0) {
+			$sql = "SELECT * FROM site_users WHERE username='$username'";
+			$result = mysqli_query($conn, $sql);
+			if (!$result->num_rows > 0) {
+				$sql = "INSERT INTO site_users (userid, firstName, lastName, username, email, phoneNumber, password) VALUES ('$uid','$firstName', '$lastName', '$username', '$email', '$phoneNumber', '$password')";
+				$result = mysqli_query($conn, $sql);
+				if ($result) {
+					header ("Location: login.php");
+				} else {
+					// Something went wrong
+                    echo "<script>alert('Something went wrong - if this error persists please contact us.')</script>";
+				}
+			} else {
+				// This username is already taken. (Modal) 
+				echo "<script>alert('!USERNAME!')</script>";
+			}
+		} else {
+			// A user with this email is already registered. (Modal)
+		}
+		
+	} else {
+		// Passwords do not match. (Modal)
+	}
+	// Send email to user with verification link.
+} else {
+	// Something went wrong - if this error persists please contact us at
+}
+
 ?>
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1"> 
-  <meta charset="utf-8">
-  <title>Signup | Healthy Connections</title>
-  <link rel="stylesheet" href="style/main.css">
-  <link rel="stylesheet" href="style/login.css">
+	<!-- REQUIRED META -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- STYLE -->
+	<link rel="stylesheet" type="text/css" href="style/login.css">
+	<link rel="stylesheet" type="text/css" href="style/main.css">
+
+	<!-- SEO -->
+	<title>Signup | Healthy Connections</title>
 </head>
 <body>
-
-<div class="container">
-
-	<?php
-
-	//if form has been submitted process it
-	if(isset($_POST['submit'])){
-
-		//collect form data
-		extract($_POST);
-
-		$sdrn = random_int(100000, 999999);
-		$uid = time().$sdrn;
-
-		//very basic validation
-		if($firstName ==''){
-			$error[] = 'Please enter your firstname.';
-		}
-
-		if($lastName ==''){
-			$error[] = 'Please enter your lastname.';
-		}
-
-		if($username ==''){
-			$error[] = 'Please enter a username.';
-		}
-
-		$usernameCheck = $db->prepare('SELECT username FROM site_users WHERE username = :username');
-		$usernameCheck->execute(array(':username' => $username));
-		$result = $usernameCheck->fetch();
-		if(!$result['username'] == ''){
-			$error[] = 'Username already exists.';
-		}
-
-		if($password ==''){
-			$error[] = 'Please enter the password.';
-		}
-
-		if($passwordConfirm ==''){
-			$error[] = 'Please confirm the password.';
-		}
-
-		if($password != $passwordConfirm){
-			$error[] = 'Passwords do not match.';
-		}
-
-		if($email ==''){
-			$error[] = 'Please enter your email address.';
-		}
-
-		$emailCheck = $db->prepare('SELECT username FROM site_users WHERE email = :email');
-		$emailCheck->execute(array(':email' => $email));
-		$eresult = $emailCheck->fetch();
-		if(!$eresult['email'] == ''){
-		 	$error[] = 'Email already registered.';
-		}
-
-		if($phoneNumber ==''){
-			$error[] = 'Please enter your phone number.';
-		}
-
-		$phoneCheck = $db->prepare('SELECT username FROM site_users WHERE phoneNumber = :phoneNumber');
-		$phoneCheck->execute(array(':phoneNumber' => $phoneNumber));
-		$presult = $phoneCheck->fetch();
-		if(!$presult['phoneNumber'] == ''){
-		 	$error[] = 'Phone Number already registered.';
-		}
-
-		if(!isset($error)){
-
-			$hashedpassword = password_hash($password, PASSWORD_BCRYPT);
-			$loginTime = time();
-
-			try {
-
-				//insert into database
-				$stmt = $db->prepare('INSERT INTO site_users (userid,username,password,email,phoneNumber) VALUES (:userid, :username, :password, :email, :phoneNumber)') ;
-				$stmt->execute(array(#
-					':userid' => $uid,
-					':username' => $username,
-					':password' => $hashedpassword,
-					':email' => $email,
-					':phoneNumber' => $phoneNumber,
-				));
-
-				//redirect to login page
-				header('Location: login.php');
-				exit;
-
-			} catch(PDOException $e) {
-			    echo $e->getMessage();
-			}
-
-		}
-
-	}
-
-	//check for any errors
-	if(isset($error)){
-		foreach($error as $error){
-			echo '<p class="error">'.$error.'</p>';
-		}
-	}
-	?>
-	<div class="form-body">
-	<h1>Signup</h1>
-	<form action='' method='post'>
-		<p><label>First Name</label><br />
-		<input required type='text' name='firstName' placeholder="Jane" value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
-		
-		<p><label>Last Name</label><br />
-		<input required type='text' name='lastName' placeholder="Doe" value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
-
-		<p><label>Username</label><br>
-		<input required type='text' name='username' placeholder="username" pattern="^[A-Za-z][A-Za-z0-9_]{2,29}$" value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
-
-		<p><label>Email</label><br />
-		<input required type='email' name='email' placeholder="jane@doe.com" value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
-
-		<p><label>Phone Number</label><br />
-		<input required id="phone" type='tel' name='phoneNumber' placeholder="+447123456789" pattern="^\+[1-9]\d{1,14}$" value='<?php if(isset($error)){ echo $_POST['phoneNumber'];}?>'></p>
-
-		<p><label>Password</label><br />
-		<input required type='password' name='password' placeholder="password" value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
-
-		<p><label>Confirm Password</label><br />
-		<input required type='password' name='passwordConfirm' placeholder="password" value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
-		
-		<p><input class="login" type='submit' name='submit' value='Sign Up'></p>
-
-	</form>
-	</div>	
-
-</div>
+	<div class="container">
+		<div class="form-body">
+        <h1 class="login-text">SignUp</h1>
+			<form action="" method="POST">
+				<div class="input-group">
+					<input type="text" placeholder="Firstname" name="firstName" required>
+					<input type="text" placeholder="Lastname" name="lastName" required>
+				</div>
+				<div class="input-group">
+					<input type="text" pattern="^[A-Za-z][A-Za-z0-9_]{2,29}$" placeholder="Username" name="username" required>
+				</div>
+				<div class="input-group">
+					<input type="email" placeholder="Email" name="email" required>
+				</div>
+                <div class="input-group">
+					<input pattern="^\+[1-9]\d{1,14}$" type="tel" placeholder="+447123456789" name="phoneNumber" required>
+				</div>
+				<div class="input-group">
+					<input type="password" placeholder="Password" name="password" required>
+				</div>
+				<div class="input-group">
+					<input type="password" placeholder="Confirm Password" name="cpassword" required>
+				</div>
+				<div class="input-group">
+                    <input class="login" type='submit' name='submit' value='Sign Up'>
+				</div>
+				<p class="login-register-text">Have an account? <a href="login.php">Login Here</a>.</p>
+			</form>
+		</div>
+	</div>
 </body>
- </html>
+</html>
