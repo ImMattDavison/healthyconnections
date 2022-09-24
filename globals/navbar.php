aad73237b55ad1a13a894a5615a9717b49b867f1
Matt <matt@spectrebot.net>
2022-09-24 17:17:16 +0100

    add account subnav

--- globals/navbar.php
+++ globals/navbar.php
@@ -20,7 +20,7 @@
                         <a class="nav-link br" href="../">ABOUT</a>
                     </div>
                 </li>
-                <?php if(!isset($_SESSION['loggedin'])){ ?>
+                <?php if(!isset($_SESSION['username'])){ ?>
                     <div class="accounts-buttons">
                         <li class="nav-item br account-button-container">
                             <a class="nav-link br register-button" href="/signup.php">SIGN-UP</a>
@@ -29,11 +29,14 @@
                             <a class="nav-link br login-button" href="/login.php">LOGIN</a>
                         </li>
                     </div>
-                <?php }else if(isset($_SESSION['loggedin'])){?>
+                <?php }else if(isset($_SESSION['username'])){?>
                     <li class="nav-item br sub-nav-parent">
                         <div class="sub-nav-closed-row">
                             <a class="nav-link br" href="../">ACCOUNT <b class="lg-dropper">&#9207;</b></a><button class="sub-nav-toggle">&#9207;</button>
                         </div>
+                        <ul class="sub-nav">
+                            <li class="sub-nav-item"><a href="logout.php">Logout</a></li>
+                        </ul>
                     </li>
                 <?php };?>
             </ul>
