2a8e5d9d2512250bc36f27c25ce1f6d48cd58e86
Matt <matt@spectrebot.net>
2022-09-25 11:04:02 +0100

    create job page

---
+++ job.php
@@ -0,0 +1,49 @@
+<?php require('includes/config.php'); 
+// if not logged in redirect to login page
+error_reporting(0);
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header('Location: login.php');
+    exit();
+}
+$postid = $_GET['id'];
+$pgContent = 'SELECT * FROM posts WHERE id = '.$postid.'';
+$gather = mysqli_query($conn, $pgContent);
+$gatheredContent = mysqli_fetch_assoc($gather);
+
+$getuser = mysqli_query($conn, "SELECT * FROM site_users WHERE username='".$_SESSION['username']."'");
+$userdata = mysqli_fetch_assoc($getuser);
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <title>Job Listing | Healthy Connections</title>
+        <link rel="stylesheet" href="style/main.css">
+        <link rel="stylesheet" href="style/post.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Job Listing</h1>
+            <div class="row poster-block">
+                <div>
+                    <p>Job Listing by: <b><?php echo $gatheredContent['username']; ?></b></p> 
+                </div>
+                <div>
+                    Posted on <?php echo date('d/m/y H:i', strtotime($gatheredContent['trialPostDate'])); ?>
+                </div>
+            </div>
+            <div>
+                <h2><?php echo $gatheredContent['trialName']; ?></h2>
+            </div>
+            <div>
+                <p><?php echo $gatheredContent['trialDesc']; ?></p>
+            </div>
+            <a href="jobapplication.php?id=<?php echo $gatheredContent['id'];?>" class="submit">Apply for this Trial</a>
+            <p class="form-info">By clicking the above button you accept for Healthy Connections to text the poster of this job opening with your full name, email address and phone number.</p>
+        </div>
+    </body>
+</html>
