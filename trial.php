354e41eab9208dc646d5b503ef533ce609453d82
Matt <matt@spectrebot.net>
2022-09-25 09:12:55 +0100

    basic trial page formatting

--- trial.php
+++ trial.php
@@ -11,7 +11,25 @@ $pgContent = 'SELECT * FROM posts WHERE id = '.$postid.'';
 $gather = mysqli_query($conn, $pgContent);
 $gatheredContent = mysqli_fetch_assoc($gather);
 
-if($gatheredContent['postType'] = 2){
-   header('Location: job.php?'.$gatheredContent['postid'].'id='.$postid.'');
-}
- 
+$getuser = mysqli_query($conn, "SELECT * FROM site_users WHERE username='".$_SESSION['username']."'");
+$userdata = mysqli_fetch_assoc($getuser);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div>
+                    <p>Medical Trial Listing by: <?php echo $gatheredContent['username']; ?></p>
+                    <small>Member of Healthy Connections since <?php echo $userdata['joinDate']; ?></small> 
+                </div>
+                <div>
+                    Posted on <?php echo date('d/m/y H:i', strtotime($gatheredContent['trialPostDate'])) ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
